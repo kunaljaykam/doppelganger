5198c357d724e245bab2148699b5aa84046f8c7d
Kunal Jaykam <50500283+kunaljaykam@users.noreply.github.com>
2022-12-02 14:22:33 +0530

    print the files that contains the duplicate id's

--- find-duplicate-ids.sh
+++ find-duplicate-ids.sh
@@ -3,5 +3,8 @@
 ## Determine the paths to the files to be checked
 cd sakai/portal
 
-## duplicate ids
+## print duplicate ids
 find . -type f -regex '.*\.\(vm\|html\|js\|hbs\|jsp\)' -exec grep -oP '(?<=id=")[^"]*' {} \; | sort | uniq -d | sort -nr | uniq -c | awk '{print $2}' | sort -u | uniq -c | awk '{print $2}' | sort -u | uniq -c | awk '{print $2}' | sort -
+
+## print the files that contain the duplicate ids
+# find . -type f -regex '.*\.\(vm\|html\|js\|hbs\|jsp\)' -exec grep -oP '(?<=id=")[^"]*' {} \; | sort | uniq -d | sort -nr | uniq -c | awk '{print $2}' | sort -u | uniq -c | awk '{print $2}' | sort -u | uniq -c | awk '{print $2}' | sort -u | while read line; do echo $line; find . -type f -regex '.*\.\(vm\|html\|js\|hbs\|jsp\)' -exec grep -H $line {} \; | awk -F: '{print $1}'; done
